310ef679646cc69dd7ba918215f7792cd7ff0004
obara.takao <obara.takao@dena.jp>
2014-04-28 08:24:55 +0900

    rewrite dependency logic

--- test/test.js
+++ test/test.js
@@ -1,7 +1,9 @@
 new Test().add([
-        testNodeModule_collectModuleData,
+        testNodeModule_collectPackageData,
+        testNodeModule_resolveDependencyTree,
+        testNodeModule_files,
     ]).run(function(err, test) {
-        if (1) {
+        if (0) {
             err || test.worker(function(err, test) {
                 if (!err && typeof NodeModule_ !== "undefined") {
                     var name = Test.swap(NodeModule, NodeModule_);
@@ -14,17 +16,49 @@ new Test().add([
         }
     });
 
-function testNodeModule_collectModuleData(next) {
+function testNodeModule_collectPackageData(next) {
+    var param = { develop: true, ignore: ["uupaa.watch.js", "uupaa.plato.js", "uupaa.minify.js"] };
+    var data  = NodeModule.collectPackageData(param); // { json, tree, list }
 
-    var result = NodeModule.collectModuleData({ develop: true });
+    console.log( "data: " + JSON.stringify(data, null, 2) );
 
-//  console.log( JSON.stringify(result, null, 2) );
-
-    if (result.nodeFiles.join(",").indexOf("uupaa.valid.js") >= 0) {
-        console.log("testNodeModule_load ok");
+    if (1) {
+        console.log("testNodeModule_collectPackageData ok");
         next && next.pass();
     } else {
-        console.error("testNodeModule_load ng");
+        console.error("testNodeModule_collectPackageData ng");
+        next && next.miss();
+    }
+}
+
+function testNodeModule_resolveDependencyTree(next) {
+    var param = { develop: true, ignore: ["uupaa.watch.js", "uupaa.plato.js", "uupaa.minify.js"] };
+    var data  = NodeModule.collectPackageData(param); // { json, tree, list }
+    var tree  = NodeModule.resolveDependencyTree(data); // { develop, release }
+
+    console.log( "data.tree: " + JSON.stringify(data.tree, null, 2) );
+    console.log( "tree.develop: " + JSON.stringify(tree.develop, null, 2) );
+
+    if (1) {
+        console.log("testNodeModule_resolveDependencyTree ok");
+        next && next.pass();
+    } else {
+        console.error("testNodeModule_resolveDependencyTree ng");
+        next && next.miss();
+    }
+}
+
+function testNodeModule_files(next) {
+    var param = { develop: true, ignore: ["uupaa.watch.js", "uupaa.plato.js", "uupaa.minify.js"] };
+    var files = NodeModule.files(param);
+
+    console.log( "files: " + JSON.stringify(files, null, 2) );
+
+    if (1) {
+        console.log("testNodeModule_files ok");
+        next && next.pass();
+    } else {
+        console.error("testNodeModule_files ng");
         next && next.miss();
     }
 }
