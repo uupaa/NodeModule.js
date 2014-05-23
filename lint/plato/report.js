35077537e3da3d5c6c58a770fe94ddb2bf5ea634
uupaa <uupaa.js@gmail.com>
2014-05-24 02:06:12 +0900

    fix Array#sort doesnot stable sort

---
+++ lint/plato/report.js
@@ -0,0 +1,63 @@
+__report = {
+  "reports": [
+    {
+      "info": {
+        "file": "lib/NodeModule.js",
+        "fileShort": "lib/NodeModule.js",
+        "fileSafe": "lib_NodeModule_js",
+        "link": "files/lib_NodeModule_js/index.html"
+      },
+      "jshint": {
+        "messages": 8
+      },
+      "complexity": {
+        "aggregate": {
+          "line": 1,
+          "complexity": {
+            "sloc": {
+              "physical": 454,
+              "logical": 237
+            },
+            "cyclomatic": 69,
+            "halstead": {
+              "operators": {
+                "distinct": 29,
+                "total": 623,
+                "identifiers": [
+                  "__stripped__"
+                ]
+              },
+              "operands": {
+                "distinct": 165,
+                "total": 729,
+                "identifiers": [
+                  "__stripped__"
+                ]
+              },
+              "length": 1352,
+              "vocabulary": 194,
+              "difficulty": 64.06363636363636,
+              "volume": 10275.082162636996,
+              "effort": 658259.1272736628,
+              "bugs": 3.4250273875456654,
+              "time": 36569.95151520349
+            },
+            "params": 52
+          }
+        },
+        "module": "lib/NodeModule.js",
+        "maintainability": 64.44991415062323
+      }
+    }
+  ],
+  "summary": {
+    "total": {
+      "sloc": 454,
+      "maintainability": 64.44991415062323
+    },
+    "average": {
+      "sloc": 454,
+      "maintainability": "64.45"
+    }
+  }
+}
--- test/index.node.js
+++ test/index.node.js
@@ -1,6 +1,9 @@
+require("../node_modules/uupaa.reflection.js/lib/Reflection.js");
+require("../lib/NodeModule.js");
+require("../node_modules/uupaa.console.js/lib/Console.js");
+require("../node_modules/uupaa.help.js/lib/Help.js");
 require("../node_modules/uupaa.valid.js/lib/Valid.js");
 require("../node_modules/uupaa.task.js/lib/Task.js");
 require("../node_modules/uupaa.test.js/lib/Test.js");
-require("../lib/NodeModule.js");
 require("../release/NodeModule.min.js");
 require("./test.js");
